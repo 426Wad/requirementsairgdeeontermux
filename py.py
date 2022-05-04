47961c5601944d679fd1c1b65e52b79e05b3e34f
Genesis <82351533+426Wad@users.noreply.github.com>
2022-05-04 12:51:33 +0800

    Add files via upload

---
+++ py.py
@@ -0,0 +1,20 @@
+import os 
+
+def install_tools():
+    os.system("pkg install root-repo")
+    os.system("pkg install aircrack-ng")
+    os.system("pkg install iw")
+    os.system("pkg install xterm")
+    os.system("pkg install ip")
+    os.system("pkg install lspci")
+    os.system("pkg install ps")
+    os.system("pkg install bettercap")
+    os.system("pkg install ettercap")
+    os.system("pkg install dnsmasq")
+    os.system("pkg install hostapd-wpe")
+    os.system("pkg install iptables")
+    os.system("pkg install bully")
+    os.system("pkg install pixiewps")
+    os.system("pkg install isc-dhcp-server")
+    os.system("pkg install asleap")
+    os.system("pkg install packetforge-ng")
